3fc384a8faf1b6a6a8f60802cd54f7d8dc31363d
zhenyang <zhenyang@sliver.tv>
2018-06-12 19:06:34 -0700

    Show more data on account page
    Need to revise later

--- backend/db/account-dao.js
+++ backend/db/account-dao.js
@@ -49,7 +49,7 @@ module.exports = class AccountDAO {
         accountInfo.address = record.bins.address;
         accountInfo.balance = record.bins.balance;
         accountInfo.sequence = record.bins.sequence;
-        // accountInfo.reserved_funds = record.bins.reserved_funds;
+        accountInfo.reserved_funds = record.bins.reserved_funds;
         accountInfo.last_updated_block_height = record.bins.lst_updt_blk;
         callback(error, accountInfo);
       }
