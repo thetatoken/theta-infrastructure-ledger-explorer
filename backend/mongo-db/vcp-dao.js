63f3aa929eb28878af8c264aa0cb40434b98e8f6
zhenyang <zhenyang@sliver.tv>
2019-02-11 14:50:36 -0800

    Add vcp dao and getvcp call

---
+++ backend/mongo-db/vcp-dao.js
@@ -0,0 +1,51 @@
+var path = require('path');
+//------------------------------------------------------------------------------
+//  DAO for vcp
+//------------------------------------------------------------------------------
+
+module.exports = class VcpDAO {
+
+  constructor(execDir, client) {
+    // this.aerospike = require(path.join(execDir, 'node_modules', 'mongodb'));
+    this.client = client;
+    this.vcpInfoCollection = 'vcp';
+  }
+
+  upsertVcp(vcpInfo, callback) {
+    const queryObject = { '_id': vcpInfo.source };
+    const newObject = {
+      'source': vcpInfo.source,
+      'stakes': vcpInfo.stakes
+    };
+    this.client.upsert(this.vcpInfoCollection, queryObject, newObject, callback);
+  }
+
+  getVcp(source, callback) {
+    const queryObject = { '_id': source };
+    this.client.findOne(this.vcpInfoCollection, queryObject, function (error, record) {
+      if (error) {
+        console.log('ERR - ', error, height);
+        // callback(error);
+      } else if (!record) {
+        callback(Error('NOT_FOUND - ' + height));
+      } else {
+        console.log(record);
+        var vcpInfo = {};
+        vcpInfo.source = record.source;
+        vcpInfo.stakes = record.stakes;
+        callback(error, vcpInfo);
+      }
+    })
+  }
+
+  checkVcp(source, callback) {
+    const queryObject = { '_id': source };
+    return this.client.exist(this.vcpInfoCollection, queryObject, function (err, res) {
+      if (err) {
+        console.log('error in checkVcp: ', err);
+        callback(err);
+      }
+      callback(err, res);
+    });
+  }
+}
