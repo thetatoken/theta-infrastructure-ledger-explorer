7afcdc7ffea5574b2482c40a7a376d831eb90aaa
zhenyang <zhenyang@sliver.tv>
2019-03-06 15:17:36 -0800

    Minor fix

--- backend/crawler/helper/transactions.js
+++ backend/crawler/helper/transactions.js
@@ -1,9 +1,11 @@
 exports.getBriefTxs = function (txs) {
+    const res = []
     txs.forEach(tx => {
         tx = {
             hash: tx.hash,
             type: tx.type
         }
+        res.push(tx);
     });
-    return txs;
+    return res;
 }
