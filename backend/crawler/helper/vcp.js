6f1596b2c9b56529e1a557383eedcd283dd70c37
zhenyang <zhenyang@sliver.tv>
2019-11-11 13:38:10 -0800

    Fix vcp helper

--- backend/crawler/helper/vcp.js
+++ backend/crawler/helper/vcp.js
@@ -3,18 +3,15 @@ exports.updateVcp = async function (candidate, vcpDao) {
   const stakes = candidate.Stakes;
   const isExist = await vcpDao.checkVcpAsync(holder);
   const vpc = isExist ? await vcpDao.getVcpAsync(holder) : null;
-  // console.log('vpc:', vpc)
-  let stakeList = vpc ? vpc.stakes : {};
-  // console.log(stakeList)
+  let stakeList = {};
   stakes.forEach(stake => {
-    // console.log(stake)
-    if (!stakeList[stake.Source]) {
-      stakeList[stake.Source] = stake.Amount;
+    if (!stakeList[stake.source]) {
+      stakeList[stake.source] = stake.amount;
     } else {
-      if (!stake.Withdrawn) {
-        stakeList[stake.Source] = stake.Amount;
+      if (!stake.withdrawn) {
+        stakeList[stake.source] = stake.amount;
       } else {
-        stakeList[stake.Source] = null;
+        stakeList[stake.source] = null;
       }
     }
   });
