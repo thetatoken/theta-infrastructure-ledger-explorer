7de68336eba15e2f2373fb710607f21bc5c5d8cb
zhenyang <zhenyang@sliver.tv>
2019-12-04 18:06:52 -0800

    Add vcp api

--- frontend/src/common/services/vcp.js
+++ frontend/src/common/services/vcp.js
@@ -3,5 +3,11 @@ import { apiService } from './api';
 export const vcpService = {
   getAllVcp() {
     return apiService.get(`vcp/all`, {});
+  },
+  getVcpByAddress(address) {
+    if (!address) {
+      throw Error('Missing argument');
+    }
+    return apiService.get(`vcp/${address}`, {});
   }
 };
