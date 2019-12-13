50de77f2a5a6710e19ac51da161097f727f3de85
zhenyang <zhenyang@sliver.tv>
2019-12-13 12:09:03 -0800

    Update stake.js

--- backend/crawler/helper/stake.js
+++ backend/crawler/helper/stake.js
@@ -3,17 +3,15 @@ exports.updateStake = async function (candidate, type, stakeDao) {
   const stakes = candidate.Stakes;
   let insertList = [];
   stakes.forEach(stake => {
-    if (!stake.withdrawn) {
-      const stakeInfo = {
-        'type': type,
-        'holder': holder,
-        'source': stake.source,
-        'amount': stake.amount,
-        'withdrawn': stake.withdrawn,
-        'return_height': stake.return_height
-      }
-      insertList.push(stakeDao.insertAsync(stakeInfo));
+    const stakeInfo = {
+      'type': type,
+      'holder': holder,
+      'source': stake.source,
+      'amount': stake.amount,
+      'withdrawn': stake.withdrawn,
+      'return_height': stake.return_height
     }
+    insertList.push(stakeDao.insertAsync(stakeInfo));
   });
   await Promise.all(insertList);
 }
