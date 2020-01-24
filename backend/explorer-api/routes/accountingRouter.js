2824ac8de3f450d12220f020000bba7ffa67b598
Qinwei Gong <qinwei@sliver.tv>
2020-01-24 15:42:00 -0800

    fix async

--- backend/explorer-api/routes/accountingRouter.js
+++ backend/explorer-api/routes/accountingRouter.js
@@ -6,15 +6,28 @@ var accountingRouter = (app, accountingDao) => {
     router.use(bodyParser.urlencoded({ extended: true }));
 
     router.get("/accounting", (req, res) => {
-        console.log("===> wallet: " + req.query.wallet)
         let wallet = req.query.wallet;
         let startDate = (Date.parse(req.query.start) / 1000).toString();
         let endDate = (Date.parse(req.query.end) / 1000).toString();
 
-        (async ()=>{
-            let data = await accountingDao.getAsync(wallet, startDate, endDate);
+        accountingDao.getAsync(wallet, startDate, endDate).then(data => {
+            // const data = ({
+            //   type: 'account',
+            //   body: accountInfo,
+            // });
             res.status(200).send(data);
-        })();
+        })
+        .catch(error => {
+            if (error.message.includes('NOT_FOUND')) {
+              const err = ({
+                type: 'error_not_found',
+                error
+              });
+              res.status(404).send(err);
+            } else {
+              console.log('ERR - ', error)
+            }
+        });
     });
 
     //the / route of router will get mapped to /api
