f37637f62cb3ad715f83c053b7240a19052f0411
Qinwei Gong <qinwei@sliver.tv>
2020-01-27 13:55:38 -0800

    fix dates

--- backend/explorer-api/routes/accountingRouter.js
+++ backend/explorer-api/routes/accountingRouter.js
@@ -7,10 +7,20 @@ var accountingRouter = (app, accountingDao) => {
 
     router.get("/accounting", (req, res) => {
         let wallet = req.query.wallet;
-        let startDate = Date.parse(req.query.start);
-        let endDate = Date.parse(req.query.end);
+        let startTime = Date.parse(req.query.start);
+        let endTime = Date.parse(req.query.end);
+        let endDate = new Date(endTime);
+        endDate.setDate(endDate.getDate() + 1);
+        endTime = endDate.getTime();
 
-        accountingDao.getAsync(wallet, startDate, endDate).then(data => {
+        accountingDao.getAsync(wallet, startTime, endTime).then(data => {
+            data.forEach(function(info) {
+                let date = new Date(info.date);
+                let month = date.getUTCMonth() + 1;
+                let day = date.getUTCDate();
+                let year = date.getUTCFullYear();
+                info.date = year + "-" + month + "-" + day;
+            });
             res.status(200).send(data);
         })
         .catch(error => {
