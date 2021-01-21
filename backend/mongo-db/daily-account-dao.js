6480215688ab561c85dbfe3deed73542ef1b1c59
zhenyang <zhenyang@sliver.tv>
2021-01-21 11:26:57 -0800

    Add daily account dao

---
+++ backend/mongo-db/daily-account-dao.js
@@ -0,0 +1,44 @@
+//------------------------------------------------------------------------------
+//  DAO for daily account
+//------------------------------------------------------------------------------
+
+module.exports = class DailyAccountDAO {
+
+  constructor(execDir, client) {
+    this.client = client;
+    this.collection = 'dailyAct';
+  }
+
+  insert(info, callback) {
+    const object = { '_id': info.address };
+    this.client.insert(this.collection, object, callback);
+  }
+  checkAccount(address, callback) {
+    const queryObject = { '_id': address };
+    return this.client.exist(this.collection, queryObject, function (err, res) {
+      if (err) {
+        console.log('error in checkAccount: ', err);
+        callback(err);
+      }
+      callback(err, res);
+    });
+  }
+  getTotalNumber(callback) {
+    this.client.getTotal(this.collection, null, function (error, record) {
+      if (error) {
+        console.log('ERR - ', error);
+      } else {
+        callback(error, record);
+      }
+    });
+  }
+  removeAll(callback) {
+    this.client.remove(this.collection, function (err, res) {
+      if (err) {
+        console.log('ERR - ', err, height);
+        callback(err);
+      }
+      callback(err, res);
+    })
+  }
+}
