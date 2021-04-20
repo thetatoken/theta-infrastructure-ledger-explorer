5c40f05a3316b5c4e42d65e2c2f3a86917323bc3
zhenyang <zhenyang@sliver.tv>
2021-04-20 14:24:24 -0700

    Cache stake records at server level

--- backend/crawler/helper/stake.js
+++ backend/crawler/helper/stake.js
@@ -1,4 +1,28 @@
 var helper = require('./utils');
+var stakesCache = {
+  vcp: new Map(),
+  gcp: new Map(),
+  eenp: new Map()
+}
+var stakeKeysCache = {
+  vcp: new Set(),
+  gcp: new Set(),
+  eenp: new Set()
+}
+
+function shallowEqual(object1, object2) {
+  const keys1 = Object.keys(object1);
+  const keys2 = Object.keys(object2);
+  if (keys1.length !== keys2.length) {
+    return false;
+  }
+  for (let key of keys1) {
+    if (object1[key] !== object2[key]) {
+      return false;
+    }
+  }
+  return true;
+}
 
 exports.updateStake = async function (candidate, type, stakeDao) {
   const holder = candidate.Holder;
@@ -19,9 +43,52 @@ exports.updateStake = async function (candidate, type, stakeDao) {
   await Promise.all(insertList);
 }
 exports.updateStakes = async function (candidateList, type, stakeDao) {
-  console.log('before update stakes:', type)
-  await stakeDao.updateStakesAsync(candidateList, type);
-  console.log('after update stakes:', type)
+  // console.log('before update stakes:', type)
+  // await stakeDao.updateStakesAsync(candidateList, type);
+  // console.log('after update stakes:', type)
+  let cacheKeyRef = stakeKeysCache[`${type}`];
+  let cacheRef = stakesCache[`${type}`];
+  if (cacheKeyRef.size === 0) {
+    await stakeDao.removeRecordsAsync(type);
+  }
+  let updateStakeList = [];
+  let existKeys = new Set(cacheKeyRef);
+  for (let candidate of candidateList) {
+    const holder = candidate.Holder;
+    const stakes = candidate.Stakes;
+    for (let stake of stakes) {
+      const id = `${type}_${holder}_${stake.source}`;
+      const stakeInfo = {
+        '_id': id,
+        'type': type,
+        'holder': holder,
+        'source': stake.source,
+        'amount': stake.amount,
+        'withdrawn': stake.withdrawn,
+        'return_height': stake.return_height
+      }
+      if (existKeys.has(id)) {
+        existKeys.delete(id);
+        if (!shallowEqual(cacheRef.get(id), stakeInfo)) {
+          updateStakeList.push(stakeInfo);
+        }
+      } else {
+        updateStakeList.push(stakeInfo);
+        cacheKeyRef.add(id);
+        cacheRef.set(id, stakeInfo);
+      }
+    };
+  }
+  let deleteKeys = [...existKeys];
+  for (let stake of updateStakeList) {
+    await stakeDao.insertAsync(stake);
+  }
+  console.log('updateStakeList length:', updateStakeList.length, 'type:', type)
+  console.log('delete keys length:', deleteKeys.length, 'type:', type);
+  await stakeDao.removeRecordsByIdAsync(type, deleteKeys);
+  for (let key of deleteKeys) {
+    cacheRef.delete(key);
+  }
 }
 exports.updateTotalStake = function (totalStake, progressDao) {
   let totalTheta = 0, totalTfuel = 0;
