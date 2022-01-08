c30c0cdcbdc515cff2ad38213236c1506c67d8a3
zhenyang <zhenyang@sliver.tv>
2022-01-07 16:29:37 -0800

    And Theta related libs

---
+++ backend/crawler/libs/Theta.js
@@ -0,0 +1,32 @@
+var BigNumber = require('bignumber.js');
+var ThetaJS = require('./thetajs.esm');
+// import Config from '../config';
+const _chainId = "testnet";  // TODOs: read chainId from config file
+
+class Theta {
+  // static _chainId = Config.defaultThetaChainID; 
+
+  static get chainId() {
+    return _chainId;
+  }
+
+  static getTransactionFee() {
+    //10^12 TFuelWei
+    return 0.000001;
+  }
+
+  static unsignedSmartContractTx(txData, sequence) {
+    let { from, to, data, value, transactionFee, gasLimit } = txData;
+
+    const ten18 = (new BigNumber(10)).pow(18); // 10^18, 1 Theta = 10^18 ThetaWei, 1 Gamma = 10^ TFuelWei
+    const feeInTFuelWei = (new BigNumber(transactionFee)).multipliedBy(ten18); // Any fee >= 10^12 TFuelWei should work, higher fee yields higher priority
+    const senderSequence = sequence;
+    const gasPrice = feeInTFuelWei;
+
+    let tx = new ThetaJS.SmartContractTx(from, to, gasLimit, gasPrice, data, value, senderSequence);
+
+    return tx;
+  }
+}
+
+module.exports = Theta;
