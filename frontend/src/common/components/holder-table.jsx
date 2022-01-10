eaf3edc14d9c441b3f7cccdd062e533d1d50b829
zhenyang <zhenyang@sliver.tv>
2022-01-10 14:48:51 -0800

    Add pagination for holder tab

--- frontend/src/common/components/holder-table.jsx
+++ frontend/src/common/components/holder-table.jsx
@@ -1,34 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import cx from 'classnames';
 import map from 'lodash/map';
 import { Link } from "react-router-dom";
+import Pagination from "common/components/pagination";
 
+const NUM_PER_PAGE = 50;
 
 const HoldersTable = ({ holders, totalSupply, className }) => {
+  const totalPages = Math.ceil(holders.length / NUM_PER_PAGE);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [currentHolders, setCurrentHolders] = useState(holders.slice(0, NUM_PER_PAGE));
+  const handlePageChange = pageNumber => {
+    setCurrentPage(pageNumber);
+    setCurrentHolders(holders.slice((pageNumber - 1) * NUM_PER_PAGE, pageNumber * NUM_PER_PAGE));
+  }
   return (
-    <table className={cx("data txn-table", className)}>
-      <thead>
-        <tr>
-          <th className="rank">Rank</th>
-          <th className="address">Address</th>
-          <th className="quantity">Quantity</th>
-          <th className="percentage">Percentage</th>
-        </tr>
-      </thead>
-      <tbody>
-        {map(holders, (holder, i) => {
-          return (
-            <tr key={i}>
-              <td className="rank">{i + 1}</td>
-              <td>
-                <Link to={`/account/${holder.key}`}>{holder.key}</Link>
-              </td>
-              <td className="rank">{holder.value}</td>
-              <td className="rank">{(holder.value / totalSupply * 100).toFixed(4)}%</td>
-            </tr>);
-        })}
-      </tbody>
-    </table>
+    <>
+      <table className={cx("data txn-table", className)}>
+        <thead>
+          <tr>
+            <th className="rank">Rank</th>
+            <th className="address">Address</th>
+            <th className="quantity">Quantity</th>
+            <th className="percentage">Percentage</th>
+          </tr>
+        </thead>
+        <tbody>
+          {map(currentHolders, (holder, i) => {
+            return (
+              <tr key={i}>
+                <td className="rank">{(currentPage - 1) * NUM_PER_PAGE + i + 1}</td>
+                <td>
+                  <Link to={`/account/${holder.key}`}>{holder.key}</Link>
+                </td>
+                <td className="rank">{holder.value}</td>
+                <td className="rank">{(holder.value / totalSupply * 100).toFixed(4)}%</td>
+              </tr>);
+          })}
+        </tbody>
+      </table>
+      <Pagination
+        size={'lg'}
+        currentPage={currentPage}
+        totalPages={totalPages}
+        onPageChange={handlePageChange} />
+    </>
   );
 }
 
