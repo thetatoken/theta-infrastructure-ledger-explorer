05dc39651730b5c5a028cf8cb64e75538f396ebb
zhenyang <zhenyang@sliver.tv>
2022-03-03 14:42:24 -0800

    Minor fix

--- frontend/src/common/helpers/tns.js
+++ frontend/src/common/helpers/tns.js
@@ -1,10 +1,4 @@
 export function arrayUnique(array) {
-    let a = array.concat();
-    for (var i = 0; i < a.length; ++i) {
-        for (var j = i + 1; j < a.length; ++j) {
-            if (a[i] === a[j] || !a[j]) a.splice(j--, 1);
-        }
-    }
-    return a;
+  return [...new Set(array)]
 }
 
