cf854a8ed58f2352556e015ec49912456f82b686
zhenyang <zhenyang@sliver.tv>
2022-11-23 20:24:28 -0800

    Update read previous token

--- backend/crawler/jobs/read-previous-token.js
+++ backend/crawler/jobs/read-previous-token.js
@@ -140,43 +140,6 @@ async function updateTokens(txs, smartContractDao, tokenDao, tokenSummaryDao) {
           }
           break;
         case EventHashMap.TRANSFER:
-          const contractAddress = get(log, 'address').toLowerCase();
-          console.log('contractList:', JSON.stringify(contractList), '. contractAddress:', contractAddress);
-          console.log(contractList.indexOf(contractAddress) > -1);
-          if (contractList.indexOf(contractAddress) > -1) {
-            let type = '';
-            switch (contractAddress) {
-              case contractMap.TNT20TokenBank:
-                type = 'XCHAIN_TNT20'
-                break;
-              case contractMap.TNT721TokenBank:
-                type = 'XCHAIN_TNT721'
-                break;
-              case contractMap.TNT1155TokenBank:
-                type = 'XCHAIN_TNT1155'
-                break;
-              default:
-                break;
-            }
-            log = decodeLogByAbiHash(log, EventHashMap.TRANSFER);
-            const tokenId = get(log, 'decode.result.tokenId');
-            const value = tokenId !== undefined ? 1 : get(log, 'decode.result[2]');
-            const newToken = {
-              _id: tx.hash.toLowerCase() + i,
-              hash: tx.hash.toLowerCase(),
-              from: (get(log, 'decode.result[0]') || '').toLowerCase(),
-              to: (get(log, 'decode.result[1]') || '').toLowerCase(),
-              token_id: tokenId,
-              value,
-              name: get(infoMap, `${contractAddress}.name`),
-              type: type,
-              timestamp: tx.timestamp,
-              contract_address: contractAddress
-            }
-            tokenArr.push(newToken);
-            insertList.push(checkAndInsertToken(newToken, tokenDao))
-            continue;
-          }
           // If log.address === tx.receipt.ContractAddress, and the contract has not been verified
           // this record will be hanlded in the contract verification
           if (get(infoMap, `${contractAddress}.type`) === 'unknow' && contractAddress === get(tx, 'receipt.ContractAddress')) {
@@ -202,10 +165,12 @@ async function updateTokens(txs, smartContractDao, tokenDao, tokenSummaryDao) {
         case EventHashMap.TFUEL_VOUCHER_MINTED:
           if (typeof get(log, 'decode') !== "object") {
             log = decodeLogByAbiHash(log, EventHashMap.TFUEL_VOUCHER_MINTED);
+            Logger.log('Decoded TFUEL_VOUCHER_MINTED Log:', JSON.stringify(log));
+            const chainId = get(log, 'decode.result[0]').split('/')[0];
             let xTfuelInfo = {
               _id: tx.hash.toLowerCase() + i,
               hash: tx.hash.toLowerCase(),
-              from: "mainchain",
+              from: chainId,
               to: get(log, 'decode.result[1]').toLowerCase(),
               value: get(log, 'decode.result[2]'),
               type: 'XCHAIN_TFUEL',
@@ -219,11 +184,12 @@ async function updateTokens(txs, smartContractDao, tokenDao, tokenSummaryDao) {
           if (typeof get(log, 'decode') !== "object") {
             log = decodeLogByAbiHash(log, EventHashMap.TFUEL_VOUCHER_BURNED);
             Logger.log('Decoded TFUEL_VOUCHER_BURNED Log:', JSON.stringify(log));
+            const chainId = get(log, 'decode.result[0]').split('/')[0];
             let xTfuelInfo = {
               _id: tx.hash.toLowerCase() + i,
               hash: tx.hash.toLowerCase(),
               from: get(log, 'decode.result[1]').toLowerCase(),
-              to: get(log, 'decode.result[2]').toLowerCase() + '_mainchain',
+              to: get(log, 'decode.result[2]').toLowerCase() + '_' + chainId,
               value: get(log, 'decode.result[3]'),
               type: 'XCHAIN_TFUEL',
               timestamp: tx.timestamp,
@@ -232,17 +198,247 @@ async function updateTokens(txs, smartContractDao, tokenDao, tokenSummaryDao) {
             insertList.push(checkAndInsertToken(xTfuelInfo, tokenDao))
           }
           break;
-        case EventHashMap.TRANSFER_SINGLE:
+        case EventHashMap.TFUEL_TOKEN_LOCKED:
           if (typeof get(log, 'decode') !== "object") {
-            log = decodeLogByAbiHash(log, EventHashMap.TRANSFER_SINGLE);
-            Logger.log('Decoded TRANSFER_SINGLE Log:', JSON.stringify(log));
+            log = decodeLogByAbiHash(log, EventHashMap.TFUEL_TOKEN_LOCKED);
+            Logger.log('Decoded TFUEL_TOKEN_LOCKED Log:', JSON.stringify(log));
+            let xTfuelInfo = {
+              _id: tx.hash.toLowerCase() + i,
+              hash: tx.hash.toLowerCase(),
+              from: get(log, 'decode.result[1]').toLowerCase(),
+              to: get(log, 'decode.result[3]').toLowerCase() + '_' + get(log, 'decode.result[2]'),
+              value: get(log, 'decode.result[4]'),
+              type: 'XCHAIN_TFUEL',
+              timestamp: tx.timestamp,
+            }
+            tokenArr.push(xTfuelInfo);
+            insertList.push(checkAndInsertToken(xTfuelInfo, tokenDao))
+          }
+          break;
+        case EventHashMap.TFUEL_TOKEN_UNLOCKED:
+          if (typeof get(log, 'decode') !== "object") {
+            log = decodeLogByAbiHash(log, EventHashMap.TFUEL_TOKEN_UNLOCKED);
+            Logger.log('Decoded TFUEL_TOKEN_UNLOCKED Log:', JSON.stringify(log));
+            let xTfuelInfo = {
+              _id: tx.hash.toLowerCase() + i,
+              hash: tx.hash.toLowerCase(),
+              from: xChainName,
+              to: get(log, 'decode.result[1]').toLowerCase(),
+              value: get(log, 'decode.result[2]'),
+              type: 'XCHAIN_TFUEL',
+              timestamp: tx.timestamp,
+            }
+            tokenArr.push(xTfuelInfo);
+            insertList.push(checkAndInsertToken(xTfuelInfo, tokenDao))
+          }
+          break;
+        case EventHashMap.TNT20_VOUCHER_MINTED:
+          if (typeof get(log, 'decode') !== "object") {
+            log = decodeLogByAbiHash(log, EventHashMap.TNT20_VOUCHER_MINTED);
+            Logger.log('Decoded TNT20_VOUCHER_MINTED Log:', JSON.stringify(log));
+            const chainId = get(log, 'decode.result[0]').split('/')[0];
+            let tokenInfo = {
+              _id: tx.hash.toLowerCase() + i,
+              hash: tx.hash.toLowerCase(),
+              from: chainId,
+              to: get(log, 'decode.result[1]').toLowerCase(),
+              value: get(log, 'decode.result[3]'),
+              type: 'XCHAIN_TNT20',
+              timestamp: tx.timestamp,
+            }
+            tokenArr.push(tokenInfo);
+            insertList.push(checkAndInsertToken(tokenInfo, tokenDao))
+          }
+          break;
+        case EventHashMap.TNT20_VOUCHER_BURNED:
+          if (typeof get(log, 'decode') !== "object") {
+            log = decodeLogByAbiHash(log, EventHashMap.TNT20_VOUCHER_BURNED);
+            Logger.log('Decoded TNT20_VOUCHER_BURNED Log:', JSON.stringify(log));
+            const chainId = get(log, 'decode.result[0]').split('/')[0];
             let tokenInfo = {
               _id: tx.hash.toLowerCase() + i,
               hash: tx.hash.toLowerCase(),
               from: get(log, 'decode.result[1]').toLowerCase(),
-              to: get(log, 'decode.result[2]').toLowerCase(),
+              to: get(log, 'decode.result[2]').toLowerCase() + '_' + chainId,
+              value: get(log, 'decode.result[3]'),
+              type: 'XCHAIN_TNT20',
+              timestamp: tx.timestamp,
+            }
+            tokenArr.push(tokenInfo);
+            insertList.push(checkAndInsertToken(tokenInfo, tokenDao))
+          }
+          break;
+        case EventHashMap.TNT20_TOKEN_LOCKED:
+          if (typeof get(log, 'decode') !== "object") {
+            log = decodeLogByAbiHash(log, EventHashMap.TNT20_TOKEN_LOCKED);
+            Logger.log('Decoded TNT20_TOKEN_LOCKED Log:', JSON.stringify(log));
+            let tokenInfo = {
+              _id: tx.hash.toLowerCase() + i,
+              hash: tx.hash.toLowerCase(),
+              from: get(log, 'decode.result[1]').toLowerCase(),
+              to: get(log, 'decode.result[3]').toLowerCase() + '_' + get(log, 'decode.result[2]'),
+              value: get(log, 'decode.result[4]'),
+              type: 'XCHAIN_TNT20',
+              timestamp: tx.timestamp,
+            }
+            tokenArr.push(tokenInfo);
+            insertList.push(checkAndInsertToken(tokenInfo, tokenDao))
+          }
+          break;
+        case EventHashMap.TNT20_TOKEN_UNLOCKED:
+          if (typeof get(log, 'decode') !== "object") {
+            log = decodeLogByAbiHash(log, EventHashMap.TNT20_TOKEN_UNLOCKED);
+            Logger.log('Decoded TNT20_TOKEN_UNLOCKED Log:', JSON.stringify(log));
+            let tokenInfo = {
+              _id: tx.hash.toLowerCase() + i,
+              hash: tx.hash.toLowerCase(),
+              from: xChainName,
+              to: get(log, 'decode.result[1]').toLowerCase(),
+              value: get(log, 'decode.result[2]'),
+              type: 'XCHAIN_TNT20',
+              timestamp: tx.timestamp,
+            }
+            tokenArr.push(tokenInfo);
+            insertList.push(checkAndInsertToken(tokenInfo, tokenDao))
+          }
+          break;
+        case EventHashMap.TNT721_VOUCHER_MINTED:
+          if (typeof get(log, 'decode') !== "object") {
+            log = decodeLogByAbiHash(log, EventHashMap.TNT721_VOUCHER_MINTED);
+            Logger.log('Decoded TNT721_VOUCHER_MINTED Log:', JSON.stringify(log));
+            const chainId = get(log, 'decode.result[0]').split('/')[0];
+            let tokenInfo = {
+              _id: tx.hash.toLowerCase() + i,
+              hash: tx.hash.toLowerCase(),
+              from: chainId,
+              to: get(log, 'decode.result[1]').toLowerCase(),
+              token_id: get(log, 'decode.result[3]'),
+              type: 'XCHAIN_TNT721',
+              timestamp: tx.timestamp,
+            }
+            tokenArr.push(tokenInfo);
+            insertList.push(checkAndInsertToken(tokenInfo, tokenDao))
+          }
+          break;
+        case EventHashMap.TNT721_VOUCHER_BURNED:
+          if (typeof get(log, 'decode') !== "object") {
+            log = decodeLogByAbiHash(log, EventHashMap.TNT721_VOUCHER_BURNED);
+            Logger.log('Decoded TNT721_VOUCHER_BURNED Log:', JSON.stringify(log));
+            const chainId = get(log, 'decode.result[0]').split('/')[0];
+            let tokenInfo = {
+              _id: tx.hash.toLowerCase() + i,
+              hash: tx.hash.toLowerCase(),
+              from: get(log, 'decode.result[1]').toLowerCase(),
+              to: get(log, 'decode.result[2]').toLowerCase() + '_' + chainId,
+              token_id: get(log, 'decode.result[3]'),
+              type: 'XCHAIN_TNT721',
+              timestamp: tx.timestamp,
+            }
+            tokenArr.push(tokenInfo);
+            insertList.push(checkAndInsertToken(tokenInfo, tokenDao))
+          }
+          break;
+        case EventHashMap.TNT721_TOKEN_LOCKED:
+          if (typeof get(log, 'decode') !== "object") {
+            log = decodeLogByAbiHash(log, EventHashMap.TNT721_TOKEN_LOCKED);
+            Logger.log('Decoded TNT721_TOKEN_LOCKED Log:', JSON.stringify(log));
+            let tokenInfo = {
+              _id: tx.hash.toLowerCase() + i,
+              hash: tx.hash.toLowerCase(),
+              from: get(log, 'decode.result[1]').toLowerCase(),
+              to: get(log, 'decode.result[3]').toLowerCase() + '_' + get(log, 'decode.result[2]'),
+              token_id: get(log, 'decode.result[4]'),
+              type: 'XCHAIN_TNT721',
+              timestamp: tx.timestamp,
+            }
+            tokenArr.push(tokenInfo);
+            insertList.push(checkAndInsertToken(tokenInfo, tokenDao))
+          }
+          break;
+        case EventHashMap.TNT721_TOKEN_UNLOCKED:
+          if (typeof get(log, 'decode') !== "object") {
+            log = decodeLogByAbiHash(log, EventHashMap.TNT721_TOKEN_UNLOCKED);
+            Logger.log('Decoded TNT721_TOKEN_UNLOCKED Log:', JSON.stringify(log));
+            let tokenInfo = {
+              _id: tx.hash.toLowerCase() + i,
+              hash: tx.hash.toLowerCase(),
+              from: xChainName,
+              to: get(log, 'decode.result[1]').toLowerCase(),
+              token_id: get(log, 'decode.result[2]'),
+              type: 'XCHAIN_TNT721',
+              timestamp: tx.timestamp,
+            }
+            tokenArr.push(tokenInfo);
+            insertList.push(checkAndInsertToken(tokenInfo, tokenDao))
+          }
+          break;
+        case EventHashMap.TNT1155_VOUCHER_MINTED:
+          if (typeof get(log, 'decode') !== "object") {
+            log = decodeLogByAbiHash(log, EventHashMap.TNT1155_VOUCHER_MINTED);
+            Logger.log('Decoded TNT1155_VOUCHER_MINTED Log:', JSON.stringify(log));
+            const chainId = get(log, 'decode.result[0]').split('/')[0];
+            let tokenInfo = {
+              _id: tx.hash.toLowerCase() + i,
+              hash: tx.hash.toLowerCase(),
+              from: chainId,
+              to: get(log, 'decode.result[1]').toLowerCase(),
               token_id: get(log, 'decode.result[3]'),
               value: get(log, 'decode.result[4]'),
+              type: 'XCHAIN_TNT1155',
+              timestamp: tx.timestamp,
+            }
+            tokenArr.push(tokenInfo);
+            insertList.push(checkAndInsertToken(tokenInfo, tokenDao))
+          }
+          break;
+        case EventHashMap.TNT1155_VOUCHER_BURNED:
+          if (typeof get(log, 'decode') !== "object") {
+            log = decodeLogByAbiHash(log, EventHashMap.TNT1155_VOUCHER_BURNED);
+            Logger.log('Decoded TNT1155_VOUCHER_BURNED Log:', JSON.stringify(log));
+            const chainId = get(log, 'decode.result[0]').split('/')[0];
+            let tokenInfo = {
+              _id: tx.hash.toLowerCase() + i,
+              hash: tx.hash.toLowerCase(),
+              from: get(log, 'decode.result[1]').toLowerCase(),
+              to: get(log, 'decode.result[2]').toLowerCase() + '_' + chainId,
+              token_id: get(log, 'decode.result[3]'),
+              value: get(log, 'decode.result[4]'),
+              type: 'XCHAIN_TNT1155',
+              timestamp: tx.timestamp,
+            }
+            tokenArr.push(tokenInfo);
+            insertList.push(checkAndInsertToken(tokenInfo, tokenDao))
+          }
+          break;
+        case EventHashMap.TNT1155_TOKEN_LOCKED:
+          if (typeof get(log, 'decode') !== "object") {
+            log = decodeLogByAbiHash(log, EventHashMap.TNT1155_TOKEN_LOCKED);
+            Logger.log('Decoded TNT1155_TOKEN_LOCKED Log:', JSON.stringify(log));
+            let tokenInfo = {
+              _id: tx.hash.toLowerCase() + i,
+              hash: tx.hash.toLowerCase(),
+              from: get(log, 'decode.result[1]').toLowerCase(),
+              to: get(log, 'decode.result[3]').toLowerCase() + '_' + get(log, 'decode.result[2]'),
+              token_id: get(log, 'decode.result[4]'),
+              value: get(log, 'decode.result[5]'),
+              type: 'XCHAIN_TNT1155',
+              timestamp: tx.timestamp,
+            }
+            tokenArr.push(tokenInfo);
+            insertList.push(checkAndInsertToken(tokenInfo, tokenDao))
+          }
+          break;
+        case EventHashMap.TNT1155_TOKEN_UNLOCKED:
+          if (typeof get(log, 'decode') !== "object") {
+            log = decodeLogByAbiHash(log, EventHashMap.TNT1155_TOKEN_UNLOCKED);
+            Logger.log('Decoded TNT1155_TOKEN_UNLOCKED Log:', JSON.stringify(log));
+            let tokenInfo = {
+              _id: tx.hash.toLowerCase() + i,
+              hash: tx.hash.toLowerCase(),
+              from: xChainName,
+              to: get(log, 'decode.result[1]').toLowerCase(),
+              token_id: get(log, 'decode.result[2]'),
+              value: get(log, 'decode.result[3]'),
               type: 'XCHAIN_TNT1155',
               timestamp: tx.timestamp,
             }
