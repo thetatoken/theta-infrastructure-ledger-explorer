dc49bf88e78a6e9a94063c69f42b0646db8a8a12
zhenyang <zhenyang@sliver.tv>
2023-01-03 11:09:33 -0800

    Support total supply query parameter

--- backend/explorer-api/routes/supplyRouter.js
+++ backend/explorer-api/routes/supplyRouter.js
@@ -10,21 +10,29 @@ var supplyRouter = (app, progressDao, dailyTfuelBurntDao, rpc, config) => {
   // The api to get total amount of Theta
   router.get("/supply/theta", (req, res) => {
     console.log('Querying the total amount of Theta.');
-    const data = ({
+    const { q } = req.query;
+    let data = ({
       "total_supply": 1000000000,
       "circulation_supply": 1000000000
     });
+    if (q === 'totalSupply') {
+      data = '1000000000';
+    }
     res.status(200).send(data);
   });
 
   // The api to get total amount of TFuel
   router.get("/supply/tfuel", (req, res) => {
     console.log('Querying the total amount of Tfuel.');
+    const { q } = req.query;
     if (config.blockchain.networkId !== 'main_net_chain') {
-      const data = ({
+      let data = ({
         "total_supply": 5000000000,
         "circulation_supply": 5000000000
       });
+      if (q === 'totalSupply') {
+        data = '5000000000';
+      }
       res.status(200).send(data);
       return;
     }
@@ -42,6 +50,9 @@ var supplyRouter = (app, progressDao, dailyTfuelBurntDao, rpc, config) => {
             "total_supply": supply,
             "circulation_supply": supply
           })
+          if (q === 'totalSupply') {
+            data = supply;
+          }
           res.status(200).send(data);
         } catch (err) {
           res.status(400).send(err.message);
@@ -50,6 +61,35 @@ var supplyRouter = (app, progressDao, dailyTfuelBurntDao, rpc, config) => {
       }).catch(err => {
         res.status(400).send(err.message);
       })
+  });
+
+  router.get("/supply/tdrop", async (req, res) => {
+    console.log('Querying the total amount of TDrop.');
+    const { q } = req.query;
+    const totalSupplyAbi = [{
+      "inputs": [],
+      "name": "totalSupply",
+      "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }],
+      "stateMutability": "view",
+      "type": "function"
+    }];
+    const tdropAddress = '0x1336739b05c7ab8a526d40dcc0d04a826b5f8b03';
+
+    try {
+      const totalSupplyWei = await getMaxTotalSupply(tdropAddress, totalSupplyAbi);
+      const totalSupply = helper.formatCoin(totalSupplyWei).toFixed(0);
+      const data = ({
+        "total_supply": totalSupply,
+        "circulation_supply": totalSupply
+      });
+      if (q === 'totalSupply') {
+        data = totalSupply;
+      }
+      res.status(200).send(data);
+    } catch (e) {
+      res.status(400).send(e.message);
+    }
+
   });
 
   router.get("/supply/tfuel/burnt", async (req, res) => {
