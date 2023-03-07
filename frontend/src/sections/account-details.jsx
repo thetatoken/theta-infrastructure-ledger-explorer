5b89511d654d4aa8a5d37135f3d6eaae7d593e02
zhenyang <zhenyang@sliver.tv>
2023-03-06 16:38:05 -0800

    Update addresses

--- frontend/src/sections/account-details.jsx
+++ frontend/src/sections/account-details.jsx
@@ -489,8 +489,8 @@ export default class AccountDetails extends React.Component {
   }
   fetchTokenBalance = async (accountAddress) => {
     const tokenMap = {
-      // TDrop: '0x1336739B05C7Ab8a526D40DCC0d04a826b5f8B03', //address for mainnet
-      TDrop: '0x08a0c0e8EFd07A98db11d79165063B6Bc2469ADF', //address for testnet
+      TDrop: '0x1336739B05C7Ab8a526D40DCC0d04a826b5f8B03', //address for mainnet
+      // TDrop: '0x08a0c0e8EFd07A98db11d79165063B6Bc2469ADF', //address for testnet
       WTFuel: '0x4dc08b15ea0e10b96c41aec22fab934ba15c983e',
       TBill: '0x22Cb20636c2d853DE2b140c2EadDbFD6C3643a39'
     }
@@ -627,8 +627,8 @@ const Balance = ({ balance, price }) => {
 
 const Token = ({ tokenBalance }) => {
   const tokenMap = {
-    // TDrop: '0x1336739B05C7Ab8a526D40DCC0d04a826b5f8B03', //address for mainnet
-    TDrop: '0x08a0c0e8EFd07A98db11d79165063B6Bc2469ADF', //address for testnet
+    TDrop: '0x1336739B05C7Ab8a526D40DCC0d04a826b5f8B03', //address for mainnet
+    // TDrop: '0x08a0c0e8EFd07A98db11d79165063B6Bc2469ADF', //address for testnet
     WTFuel: '0x4dc08b15ea0e10b96c41aec22fab934ba15c983e',
     TBill: '0x22Cb20636c2d853DE2b140c2EadDbFD6C3643a39'
   }
