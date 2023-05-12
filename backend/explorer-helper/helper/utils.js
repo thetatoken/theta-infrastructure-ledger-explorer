172584ac8baed592235ef5834f2599f16d0b34e7
zhenyang <zhenyang@sliver.tv>
2023-05-11 21:55:22 -0700

    Support imported openzeppelin contracts

--- backend/explorer-helper/helper/utils.js
+++ backend/explorer-helper/helper/utils.js
@@ -1,4 +1,6 @@
 var { linkBytecode } = require('solc/linker');
+var fs = require('fs');
+var path = require('path');
 
 exports.normalize = function (hash) {
     const regex = /^0x/i;
@@ -24,11 +26,8 @@ exports.stampDate = function (sourceCode) {
     date = new Date(date.getTime() - (offset * 60 * 1000))
     return `/**\n *Submitted for verification at thetatoken.org on ${date.toISOString().split('T')[0]}\n */\n` + sourceCode;
 }
-exports.flatSourceCode = function (sourceCode, isSingleFile) {
-    console.log('isSingleFile in flat:', isSingleFile)
-    if (isSingleFile) return sourceCode;
-    let source = JSON.parse(sourceCode);
-    const flattenedBytecode = Object.values(source).map(contract => contract.content).join('\n');
+exports.flatSourceCode = function (sourceCode) {
+    const flattenedBytecode = Object.values(sourceCode).map(contract => contract.content).join('\n');
     let flattenedSource = linkBytecode(flattenedBytecode, {});
     flattenedSource = flattenedSource.replace(/import.*;\n/g, "");
 
@@ -39,3 +38,26 @@ exports.flatSourceCode = function (sourceCode, isSingleFile) {
     });
     return flattenedSource;
 }
+
+function _getImportedContracts(importedFiles, input) {
+    let nestedImportedContracts = [];
+
+    for (const importedFile of importedFiles) {
+        const importedSource = fs.readFileSync(`node_modules/${importedFile}`, 'utf8');
+
+        const nestedImports = importedSource.match(/import\s+"(.+\.sol)"/g) || [];
+        console.log('nestedImports:', nestedImports)
+        input.sources[importedFile] = {
+            content: importedSource
+        };
+        if (nestedImports.length > 0) {
+            const nestedImportedFiles = [...new Set(nestedImports.map((match) => path.join(path.dirname(importedFile), match.replace(/import\s+"/g, '').replace(/"/g, ''))))];
+            console.log('nestedImportedFiles:', nestedImportedFiles)
+            nestedImportedContracts.push(..._getImportedContracts(nestedImportedFiles, input));
+        }
+    }
+
+    return nestedImportedContracts;
+}
+
+exports.getImportedContracts = _getImportedContracts;
--- backend/explorer-helper/routes/smartContractRouter.js
+++ backend/explorer-helper/routes/smartContractRouter.js
@@ -5,6 +5,7 @@ var helper = require('../helper/utils');
 var solc = require('solc');
 var downloader = require('../helper/solcDownloader');
 var fs = require('fs');
+var path = require('path');
 
 var smartContractRouter = (app) => {
   router.use(bodyParser.urlencoded({ extended: true }));
@@ -24,7 +25,7 @@ var smartContractRouter = (app) => {
           content: sourceCode
         }
       } : JSON.parse(sourceCode);
-      console.log('sourcecodes:', sourcecodes);
+      // console.log('sourcecodes:', sourcecodes);
       let start = +new Date();
       var input = {
         language: 'Solidity',
@@ -41,6 +42,8 @@ var smartContractRouter = (app) => {
         },
         sources: sourcecodes
       };
+
+      // console.log('input:', input);
       var output = '';
       console.log(`Loading specific version starts.`)
       console.log(`version: ${version}`);
@@ -56,6 +59,15 @@ var smartContractRouter = (app) => {
       start = +new Date();
       const solcjs = solc.setupMethods(require('.' + fileName));
       console.log(`load solc-js version takes: ${(+new Date() - start) / 1000} seconds`)
+      start = +new Date();
+
+      const imports = sourceCode.match(/import\s+"(\@openzeppelin\/contracts\/.+\.sol)"/g) || [];
+      // console.log('imports:', imports)
+      const importedFiles = [...new Set(imports.map((match) => match.replace(/import\s+"/g, '').replace(/"/g, '')))];
+      // console.log('importedFiles:', importedFiles)
+      helper.getImportedContracts(importedFiles, input);
+      console.log('input.sources:', [...Object.keys(input.sources)])
+      console.log(`getImportedContracts takes: ${(+new Date() - start) / 1000} seconds`)
       start = +new Date();
       output = JSON.parse(solcjs.compile(JSON.stringify(input)))
       console.log(`compile takes ${(+new Date() - start) / 1000} seconds`)
@@ -78,12 +90,12 @@ var smartContractRouter = (app) => {
       if (check.error) {
         data = { result: { verified: false }, err_msg: check.error }
       } else {
-        console.log('output.contracts.MyContract.sol:', output);
+        // console.log('output.contracts.MyContract.sol:', output);
         if (output.contracts) {
           let hexBytecode = helper.getHex(byteCode).substring(2);
-          console.log('hexBytecode:', hexBytecode);
+          // console.log('hexBytecode:', hexBytecode);
           for (var cFileName in output.contracts) {
-            console.log('cFileName:', cFileName)
+            // console.log('cFileName:', cFileName)
             for (var contractName in output.contracts[cFileName]) {
               const byteCode = output.contracts[cFileName][contractName].evm.bytecode.object;
               const deployedBytecode = output.contracts[cFileName][contractName].evm.deployedBytecode.object;
@@ -91,11 +103,11 @@ var smartContractRouter = (app) => {
               // const processed_blockchain_bytecode = helper.getBytecodeWithoutMetadata(hexBytecode.slice(0, curCode.length));
               const constructor_arguments = hexBytecode.slice(byteCode.length);
               // const linkReferences = output.contracts[cFileName][contractName].evm.bytecode.linkReferences;
-              console.log(`contract name:`, contractName)
+              // console.log(`contract name:`, contractName)
               // console.log(`processed_blockchain_bytecode: length:${processed_blockchain_bytecode.length}`);
-              console.log(`processed_compiled_bytecode: length:${processed_compiled_bytecode.length}`);
-              console.log(`processed_compiled_bytecode: ${processed_compiled_bytecode}`);
-              console.log(`hexBytecode: length:${hexBytecode.length}`);
+              // console.log(`processed_compiled_bytecode: length:${processed_compiled_bytecode.length}`);
+              // console.log(`processed_compiled_bytecode: ${processed_compiled_bytecode}`);
+              // console.log(`hexBytecode: length:${hexBytecode.length}`);
               // console.log('linkReferences:', linkReferences)
               // Object.keys(linkReferences).forEach((lFName) => {
               //   Object.keys(linkReferences[lFName]).forEach(cName => {
@@ -120,7 +132,7 @@ var smartContractRouter = (app) => {
                 sc = {
                   'address': address,
                   'abi': abi,
-                  'source_code': helper.stampDate(helper.flatSourceCode(sourceCode, isSingleFile)),
+                  'source_code': helper.stampDate(helper.flatSourceCode(input.sources)),
                   'verification_date': +new Date(),
                   'compiler_version': breifVersion,
                   'optimizer': optimizer === '1' ? 'enabled' : 'disabled',
